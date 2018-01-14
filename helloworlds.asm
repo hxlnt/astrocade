468167212b054c0f5ca27cbb1dbc32825560f3de
Rachel Simone Weil <partytimehexcellent@gmail.com>
2018-01-14 03:10:35 -0600

    Remade it from scratch ;-)

--- helloworlds.asm
+++ helloworlds.asm
@@ -1,51 +1,42 @@
-; Demo #1: Hello Worlds!
-; 2018 hxlnt
+;; Demo 1: HELLO WORLDS
+;; Assemble with zmac 1.3: zmac -o helloworlds.bin helloworlds.asm
+;; 2018 hxlnt
 
-; To assemble with zmac: zmac -o titledis.bin titledis.asm
+            INCLUDE "HVGLIB.H"
+            ORG     FIRSTC      ; Initialize memory address ($2000)
+            DB      "U"         ; Set up cartridge
+            DW      MENUST      ; Astrocade menu address
+            DW      PrgName     ; Address of title for program
+            DW      PrgStart    ; Entry point if user selects title on menu screen
 
-INCLUDE     "HVGLIB.H"
-
-ORG         FIRSTC   
-DB          "U"      
-DW          MENUST
-DW          $PRGNAME        ; Address of title for program
-DW          $PRGSTART       ; Jump here if prog is selected
-
-$PRGNAME:   ASCII "Hello, worlds!"
-
-$PRGSTART:  DI
-            SYSTEM (INTPC)
-            DO (SETOUT)
-            DB $B0          ; Vertical Blanking Line
-            DB $2C          ; Left/Right Color Boundary
-            DB 00001000b    ; Set Bit 3 of INterrupt MODe
-            DO (COLSET)
-            DW $COLTAB      ; Color Table
-            DO (MOVE)
-            DW NORMEM       ; Destination
-            DW 4000D        ; Bytes to move
-            DW $BITMAP      ; Source Address
+PrgStart:   DI                  ; Initialization
+            SYSTEM  (INTPC)     
+            DO      (SETOUT)
+            DB      $B0         
+            DB      $12         ; Vertical color boundary location
+            DB      00001000B
+            DO      (COLSET)    ; Set color palettes
+            DW      Palettes
+            DO      (FILL)      ; Fill screen with background color
+            DW      NORMEM      
+            DW      4000D       
+            DB      $00         
+            DO      (STRDIS)    ; Display string on screen
+            DB      29d         ; X coordinate of string
+            DB      37d         ; Y coordinate of string
+            DB      $0C         
+            DW      PrgName     ; Address of string
             EXIT
 
-$LOOP:      JP $LOOP        ; Infinite loop
+Loop:       JP      Loop        ; Infinite loop
 
-$COLTAB:    DB $44          ; Color 3 Left
-            DB $88          ; Color 2 Left 
-            DB $00          ; Color 1 Left
-            DB $33          ; Color 0 Left 
-            DB $66          ; Color 3 Right
-            DB $99          ; Color 2 Right
-            DB $CC          ; Color 1 Right
-            DB $FF          ; Color 0 Right
+Palettes:   DB      $CF         ; Left palette: Color 3
+            DB      $2E         ; Left palette: Color 2
+            DB      $69         ; Left palette: Color 1 
+            DB      $00         ; Left palette: Color 0 
+            DB      $FF         ; Right palette: Color 3
+            DB      $4F         ; Right palette: Color 2
+            DB      $DD         ; Right palette: Color 1
+            DB      $00         ; Color 0
 
-$BITMAP:    DC 408d, 00000001b
-            DC 408d, 00000010b
-            DC 408d, 00000100b
-            DC 408d, 00000101b
-            DC 408d, 00000110b
-            DC 408d, 00000111b
-            DC 408d, 00001000b
-            DC 408d, 00001001b
-            DC 408d, 00001010b
-            DC 408d, 00001011b 
-
+PrgName:    DB      "HELLO, WORLDS", 0
