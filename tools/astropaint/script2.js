985f2212ee5f4a70cf8e0f737e47f32b27097ff8
Rachel Weil <raweil@microsoft.com>
2022-09-21 10:03:59 -0500

    WIP

---
+++ tools/astropaint/script2.js
@@ -0,0 +1,249 @@
+let canvasRow = 
+    `
+    <div class="metacol metacol-1">
+              <div class="subitem"></div>
+              <div class="subitem"></div>
+              <div class="subitem"></div>
+              <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-2">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-3">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-4">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-5">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-6">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-7">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-8">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-9">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-10">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-11">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-12">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-13">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-14">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-15">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-16">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-17">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-18">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-19">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-20">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-21">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-22">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-23">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-24">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-25">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-26">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-27">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-28">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-29">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-30">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-31">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-32">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-33">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-34">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-35">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-36">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-37">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-38">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-39">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+            <div class="metacol metacol-40">
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+                <div class="subitem"></div>
+            </div>
+    `
+
+function drawRows(rowNumber){
+    document.getElementById("grid").innerHTML = canvasRow.repeat(rowNumber);
+}
+
+drawRows(102);
